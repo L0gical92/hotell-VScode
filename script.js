5d6c97231f4c72092d03d45766a98c32a5af892b
Alwaysknocked <Kaderalrifaee@gmail.com>
2023-01-04 19:01:48 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".nav-bar");
+hamburger.onclick = function () {
+    navBar.classList.toggle("active");
+}
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
+document.getElementById("subject").addEventListener("change", function () {
+    if (this.value === "Existing-booking") {
+        document.getElementById("dateRange").style.display = "none";
+        document.getElementById("bookingNr").style.display = "block";
+    } else if (this.value === "New-booking") {
+        document.getElementById("bookingNr").style.display = "none";
+        document.getElementById("dateRange").style.display = "block";
+    }else{
+        document.getElementById("dateRange").style.display = "none";
+        document.getElementById("bookingNr").style.display = "none";
+    }
+});
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    document.getElementById("contactForm").style.display = "none"; 
+    document.getElementById("confirmationMSG").style.display = "block"; 
+});
